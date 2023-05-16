41f0523cf9fc5dbc51d5171f9ed89de97e601ba4
Lucas Israel <lisraelalves92@gmail.com>
2023-05-16 14:30:04 -0300

    feat: :sparkles: this one is complicated

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,36 @@
 # Req 3
+from models.dish import Dish
+import pandas as pd
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.df = pd.read_csv(source_path)
+        self.dishes = set()
+        self.__populate_data()
+
+    def __len__(self):
+        return len(self.dishes)
+
+    def __dict_creator(self):
+        ingredient_dict = dict()
+        recipe_amount_dict = dict()
+        for a in self.df.groupby(["dish", "price"])[
+            ["ingredient", "recipe_amount"]
+        ]:
+            ingredient_dict[a[0]] = tuple(a[1]["ingredient"])
+            recipe_amount_dict[a[0]] = tuple(a[1]["recipe_amount"])
+        return (ingredient_dict, recipe_amount_dict)
+
+    def __populate_data(self):
+        ing_di, amo_di = self.__dict_creator()
+
+        for ele in ing_di:
+            new_dish = Dish(ele[0], ele[1])
+            for ing, amount in zip(ing_di[ele], amo_di[ele]):
+                new_dish.add_ingredient_dependency(ing, amount)
+            self.dishes.add(new_dish)
+
+
+if __name__ == "__main__":
+    a = MenuData("tests/mocks/menu_base_data.csv")
