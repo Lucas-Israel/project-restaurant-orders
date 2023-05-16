c60d81c777849451373a5d112a41486602c1c681
Lucas Israel <lisraelalves92@gmail.com>
2023-05-16 18:51:28 -0300

    feat: :sparkles: in progress req 5

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -1,4 +1,4 @@
-from csv import DictReader
+from csv import DictReader, DictWriter
 from typing import Dict
 
 from src.models.dish import Recipe
@@ -20,15 +20,39 @@ def read_csv_inventory(inventory_file_path=BASE_INVENTORY) -> Dict:
     return inventory
 
 
+def write_csv_inventory(data, inventory_file_path=BASE_INVENTORY):
+    print(data)
+    with open(inventory_file_path, "w", newline="") as file:
+        header = ["ingredient", "initial_amount"]
+        writer = DictWriter(file, fieldnames=header)
+        writer.writeheader()
+        writer.writerow({"ingredient": "a", "initial_amount": 2})
+
+
 # Req 5
 class InventoryMapping:
     def __init__(self, inventory_file_path=BASE_INVENTORY) -> None:
         self.inventory = read_csv_inventory(inventory_file_path)
+        self._file_path = inventory_file_path
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe):
-        pass
+        for rec in recipe:
+            if rec not in self.inventory:
+                return False
+            if recipe[rec] > self.inventory[rec]:
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        inventario = self.inventory
+
+        if self.check_recipe_availability(recipe) is False:
+            raise ValueError()
+
+        for rec in recipe:
+            inventario[rec] -= recipe[rec]
+
+        write_csv_inventory(inventario, self._file_path)
+        return None
